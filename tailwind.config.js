e77534fa05e35b5a548d444822826adeee772c99
Ewilan Rivière <ewilan.riviere@gmail.com>
2023-07-18 14:54:08 +0200

    wip

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,25 +1,26 @@
-import defaultTheme from 'tailwindcss/defaultTheme'
+import colors from 'tailwindcss/colors'
 import forms from '@tailwindcss/forms'
 import typography from '@tailwindcss/typography'
 
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
+    './resources/**/*.blade.php',
+    './vendor/filament/**/*.blade.php',
     './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
     './vendor/laravel/jetstream/**/*.blade.php',
     './storage/framework/views/*.php',
-    './resources/views/**/*.blade.php',
   ],
-
   darkMode: 'class',
-
   theme: {
     extend: {
-      fontFamily: {
-        sans: ['Figtree', ...defaultTheme.fontFamily.sans],
+      colors: {
+        danger: colors.rose,
+        primary: colors.blue,
+        success: colors.green,
+        warning: colors.yellow,
       },
     },
   },
-
   plugins: [forms, typography],
 }
